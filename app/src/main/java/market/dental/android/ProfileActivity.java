f14de2d49581576efc41882023e9795476d24afd
Kemal S. KARACA (ULAKBIM) <kemalsami.karaca@tubitak.gov.tr>
2018-01-21 14:23:10 +0300

    #8 ** Profil Activity sayfasına başlandı && resim ve kaydet butonu eklendi

---
+++ app/src/main/java/market/dental/android/ProfileActivity.java
@@ -0,0 +1,28 @@
+package market.dental.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
