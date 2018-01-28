07360afc2b4e18954588b03087074b2e4e0c8775
Kemal S. KARACA (ULAKBIM) <kemalsami.karaca@tubitak.gov.tr>
2018-01-28 19:57:35 +0300

    #12 ** şifre yenileme linki sayfası hazırlandı

--- app/src/main/java/market/dental/android/ForgotPasswordActivity.java
+++ app/src/main/java/market/dental/android/ForgotPasswordActivity.java
@@ -15,18 +15,6 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 
